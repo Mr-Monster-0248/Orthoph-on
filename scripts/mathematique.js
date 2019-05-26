ca210c840f80f02f8d551829e55298954caf303a
Camille Briand <camille.briand@efrei.net>
2019-05-26 17:25:50 +0200

    Add "lazy reloading" to Mathematique game

--- scripts/mathematique.js
+++ scripts/mathematique.js
@@ -3,6 +3,8 @@ const $ = require("../res/jquery");
 const calculus = $("#calculus");
 const numberChoice = $("#number-choice");
 
+let currentLevel = 0;
+
 let maxNumber = 50; // for now 50 is the max may change with level design
 
 function getRandomNumber(size) {
@@ -10,12 +12,11 @@ function getRandomNumber(size) {
 }
 
 const newGame = $("#newGame");
-newGame.hide();
 
 function isGood(a, b) {
     if (a === b) {
         newGame.show();
-        calculus.html(calculus.html().replace(" ?", res));
+        calculus.html(calculus.html().replace(" ?", a + b));
     } else {
 			M.toast({html: "Mauvaise réponse... Essayez encore"});
 		}
@@ -28,27 +29,42 @@ function getCalculus () {
     return [a,b,res];
 }
 
-let [a,b,res] = getCalculus();
 
-calculus.html("<span>" + a.toString() + " + " + b.toString() + " =  ?</span>");
+function nextLevel() {
+		currentLevel++;
 
-let placeRes = getRandomNumber(3);
-for (let i=0; i < 3; i++) {
-    if (i === placeRes) {
-        let num = res;
-        numberChoice.append("<span class='col s4' id='rans'>" + res + "</span>");
+		M.Toast.dismissAll();
 
-        document.querySelector("#rans").addEventListener("click", function(e) {
-            e.stopPropagation();
-            isGood(num, res);
-        });
-    } else {
-        let num = getRandomNumber(90);
-        numberChoice.append(`<span class='col s4 wans${i}'> ${num} </span>`);
+		console.log(`Current level: ${currentLevel}`);
 
-        document.querySelector(`#number-choice span.wans${i}`).addEventListener("click", function(e) {
-            e.stopPropagation();
-            isGood(num, res);
-        })
-    }
+		newGame.hide();
+
+		numberChoice.html("");  // Clear previous answers
+
+		let [a,b,res] = getCalculus();
+
+		calculus.html("<span>" + a.toString() + " + " + b.toString() + " =  ?</span>");
+
+		let placeRes = getRandomNumber(3);
+		for (let i=0; i < 3; i++) {
+				if (i === placeRes) {
+						let num = res;
+						numberChoice.append("<span class='col s4' id='rans'>" + res + "</span>");
+
+						document.querySelector("#rans").addEventListener("click", function(e) {
+								e.stopPropagation();
+								isGood(num, res);
+						});
+				} else {
+						let num = getRandomNumber(90);
+						numberChoice.append(`<span class='col s4 wans${i}'> ${num} </span>`);
+
+						document.querySelector(`#number-choice span.wans${i}`).addEventListener("click", function(e) {
+								e.stopPropagation();
+								isGood(num, res);
+						})
+				}
+		}
 }
+
+nextLevel();
