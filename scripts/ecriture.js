a2091c1134e31ca793c5a829165edb54a911628d
Camille Briand <camille.briand@efrei.net>
2019-05-26 15:48:01 +0200

    Make Ecriture game work

--- scripts/ecriture.js
+++ scripts/ecriture.js
@@ -3,7 +3,11 @@ const $ = require("../res/jquery");
 const wordDisplay = $("#word-display span");
 const wordEntry = $("#word-entry span");
 
+let currentLetterIndex = 0;
+
 let myWord = [];
+let secretWord = "";
+let currentEntry = "";
 
 function getRandomLine(size) {
     return Math.floor(Math.random() * size);
@@ -23,17 +27,19 @@ function getWordFromFile() {
 function setWordEntry(word) {
     let size = word.length;
     let entry = "";
-    for(let i = 1; i < size; i++) {
-        entry += "_ ";
+    for(let i = 0; i < size; i++) {
+      	entry += "_ ";
     }
     return entry;
 }
 
-let secretWord = "";
+
 getWordFromFile().then((word) => {
     secretWord = word;
+		currentEntry = setWordEntry(word);
+
     wordDisplay.html(word);
-    wordEntry.html(setWordEntry(word));
+    wordEntry.html(currentEntry);
 });
 
 function buildWordtoFind(word, myWord) {
@@ -42,6 +48,52 @@ function buildWordtoFind(word, myWord) {
     }
 }
 
+
+function updateEntry(key) {
+		let entry = [];
+		let i = 0;
+		for (i = 0; i < currentLetterIndex; i++) {
+				entry.push(secretWord[i]);
+		}
+		entry.push(key.toUpperCase());
+		i++;
+		for (i; i < secretWord.length; i++) {
+			entry.push("_ ");
+		}
+
+		currentLetterIndex++;
+
+		return entry.join("");
+}
+
+
+function updateEntryWithError(key) {
+	let entry = [];
+	let i = 0;
+	for (i = 0; i < currentLetterIndex; i++) {
+		entry.push(secretWord[i]);
+	}
+	entry.push(`<span style="color: red">${key.toUpperCase()}</span> `);
+	i++;
+	for (i; i < secretWord.length; i++) {
+		entry.push("_ ");
+	}
+
+	return entry.join("");
+}
+
+
+
 document.addEventListener("keypress", (e) => {
-    console.log(e);
+		console.log("Key:", e.key.toUpperCase());
+		console.log("Looking for:", secretWord[currentLetterIndex]);
+		console.log("Match:", e.key.toUpperCase() === secretWord[currentLetterIndex]);
+
+		if (e.key.toUpperCase() === secretWord[currentLetterIndex]) {
+				currentEntry = updateEntry(e.key);
+		} else {
+			currentEntry = updateEntryWithError(e.key);
+		}
+
+		wordEntry.html(currentEntry);
 });
